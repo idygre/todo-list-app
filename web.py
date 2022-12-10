fcc3fa3c9babd88e3c5aa4cfce2d99d24faf03d5
Greydi Lora <greydi.lora@yahoo.com>
2022-12-10 18:11:27 -0500

    Finish last items

--- web.py
+++ web.py
@@ -8,15 +8,21 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
-
 todos = functions.get_todos()
 
 st.title("My Todo List")
 st.subheader("Add new todo items")
 st.write("Increase your productivity with this app")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a todo", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
+
+# st.session_state
